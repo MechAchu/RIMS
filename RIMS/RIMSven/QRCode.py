8d3bcc5cdaf903ca2156ded2217461c5e9cdb69e
MechAchu <jacob_suveges@hotmail.com>
2019-07-31 18:43:36 -0700

    Update QRCode.py

--- RIMS/RIMSven/QRCode.py
+++ RIMS/RIMSven/QRCode.py
@@ -1,1 +1,30 @@
-#Code to read QR codes and decide where to go
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+import webbrowser
+
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+
+while True:
+    _, frame = cap.read()
+
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        data = obj.data
+        print("Data", data)
+        cv2.putText(frame, str(data), (50, 50), font, 2, (255, 0, 0), 3)
+        key = cv2.waitKey(1)
+        if(key == 32):
+            i = 0
+            while(i < 1):
+                webbrowser.open_new(data)
+                i = i + 1
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+#Reads the QR code and takes you to the website when you click the spacebar
+#While loop so it only opens one instance of the website
