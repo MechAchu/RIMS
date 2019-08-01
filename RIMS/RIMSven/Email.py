a944166dc7c1e7e6af63e9ee7725fb6f82628218
MechAchu <jacob_suveges@hotmail.com>
2019-07-31 18:42:23 -0700

    Update Email.py

--- RIMS/RIMSven/Email.py
+++ RIMS/RIMSven/Email.py
@@ -1,1 +1,9 @@
-#Auto Email
+import smtplib
+
+#if(items in database are less than number):
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(user = 'beyerroboticswebsite@gmail.com', password = 'IronPatriots')
+server.sendmail('beyerroboticswebsite@gmail.com', 'jacob_suveges@hotmail.com', 'We are low on items')
+server.quit()
+#Pagani.H@monet.k12.ca.us
+#In robotics email need to set security setting
